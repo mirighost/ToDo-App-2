e21d03751943ad05667df2c4632811c1a2105498
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-04-11 15:14:14 +0400

    ToDo App 2

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+function App() {
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    const handleChange = (event) => {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    };
+
+    const addItem = () => {
+        setItems(prevItems => {
+            return [...prevItems, inputText,];
+        });
+        setInputText("");
+    };
+
+    return (
+        <div className="container">
+
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+
+            <div className="form">
+                <input onChange={handleChange} type="text" value={inputText} />
+
+                <button onClick={addItem} className="button">
+                    <span>Add</span>
+                </button>
+
+            </div>
+            <div>
+                <ul>
+                    {items.map(todoItem => (
+                        <li>{todoItem}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default App;
